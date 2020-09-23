f24c5a8363e225751bd9b2777387d8478639d73d
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-22 21:09:34 -0400

    set up query for getting links

---
+++ src/containers/LinkList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from '../components/Link';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+
+const LinkList = () => {
+  return (
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <div>Fetching</div>;
+        }
+        if (error) {
+          return <div>Error</div>;
+        }
+
+        const linksToRender = data.feed.links;
+
+        return (
+          <div>
+            {linksToRender.map((link) => (
+              <Link key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default LinkList;
